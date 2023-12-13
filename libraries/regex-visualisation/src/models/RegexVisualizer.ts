37e4bc7fdfa9cc7b2e920deab8b0c44f65f88f94
Kundros <dominik@kundra.cz>
2023-12-13 20:46:11 +0100

    commit before switching branches

--- libraries/regex-visualisation/src/models/RegexVisualizer.ts
+++ libraries/regex-visualisation/src/models/RegexVisualizer.ts
@@ -1,6 +1,7 @@
 import { WebviewApi } from "vscode-webview";
 import { TextEditor } from "./TextEditor";
 import { RegexMatch } from "@kundros/regexer";
+import { Message, MessageRegexData } from "types";
 
 export class RegexVisualizer {
     constructor(regexEditor : TextEditor, stringEditor : TextEditor, vscode : WebviewApi<unknown>)
@@ -10,14 +11,28 @@ export class RegexVisualizer {
         this.vscode_ = vscode;
 
         this.regexEditor_.bindEvent('input', (event: InputEvent, text: string) => this.regexTextCallback(event, text));
+        window.addEventListener('message', this.messageRecieve);
     }
 
-    private async regexTextCallback(event: InputEvent, text: string) 
+    private regexTextCallback(event : InputEvent, text : string) 
     {
         this.vscode_.postMessage({
             type: 'regex_update',
             data: text
         });
+    }
+
+    private messageRecieve(event : MessageEvent){
+        const message = event.data as Message;
+
+        switch(event.type){
+            case 'regex_data':
+            {
+                const RegexData = message as MessageRegexData;
+
+                break;
+            }
+        }
     }
 
     private regexEditor_ : TextEditor;
--- libraries/regex-visualisation/types/index.d.ts
+++ libraries/regex-visualisation/types/index.d.ts
@@ -1,4 +1,15 @@
+import { Regexer } from "@kundros/regexer";
+
 declare module '*.html' {
     const content: string;
     export default content;
+}
+
+export type Message = {
+    type: string,
+    data: unknown
+} 
+
+export type MessageRegexData = Message & {
+    data: Regexer
 }
