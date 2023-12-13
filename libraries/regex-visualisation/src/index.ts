365c9c99f38810ad241dbd9c382d79351272fbcd
Kundros <dominik@kundra.cz>
2023-12-13 17:41:50 +0100

    fixed vscode api include

--- libraries/regex-visualisation/src/index.ts
+++ libraries/regex-visualisation/src/index.ts
@@ -1,10 +1,15 @@
 import { RegexVisualizer } from "./models/RegexVisualizer";
 import { TextEditor } from "./models/TextEditor";
 
-const vscode = acquireVsCodeApi();
+import './styles/main.less';
 
-const regexVisualizer = new RegexVisualizer(
-    new TextEditor(document.querySelector("#regex-wrapper > span"), document.querySelector("#regex-wrapper > canvas")),
-    new TextEditor(document.querySelector("#match-wrapper > span"), document.querySelector("#match-wrapper > canvas")),
-    vscode
-);
+/** private scope vscode api variable according to: https://code.visualstudio.com/api/extension-guides/webview#passing-messages-from-a-webview-to-an-extension */
+(function() {
+    const vscode = acquireVsCodeApi();
+
+    const regexVisualizer = new RegexVisualizer(
+        new TextEditor(document.querySelector("#regex-wrapper > span"), document.querySelector("#regex-wrapper > canvas")),
+        new TextEditor(document.querySelector("#match-wrapper > span"), document.querySelector("#match-wrapper > canvas")),
+        vscode
+    );
+}());
--- libraries/regex-visualisation/src/models/RegexVisualizer.ts
+++ libraries/regex-visualisation/src/models/RegexVisualizer.ts
@@ -2,13 +2,12 @@ import { WebviewApi } from "vscode-webview";
 import { TextEditor } from "./TextEditor";
 import { RegexMatch } from "@kundros/regexer";
 
-import '../styles/main.less';
-
 export class RegexVisualizer {
     constructor(regexEditor : TextEditor, stringEditor : TextEditor, vscode : WebviewApi<unknown>)
     {
         this.regexEditor_ = regexEditor;
         this.stringEditor_ = stringEditor;
+        this.vscode_ = vscode;
 
         this.regexEditor_.bindEvent('input', (event: InputEvent, text: string) => this.regexTextCallback(event, text));
     }
