d0175959082df26bbadf0c3843ad825728797f42
Kundros <dominik@kundra.cz>
2024-01-18 16:30:24 +0100

    added browser exports

--- libraries/regexer/src/browser/index.ts
+++ libraries/regexer/src/browser/index.ts
@@ -1,3 +1,6 @@
 export * from "@browser/core/Regexer";
 export * from "@core/RegexMatch"
+export * from "@exceptions/RegParseException";
+export * from "@exceptions/RegMatchException";
 export * from "@regexer/coreTypes/parserTypes";
+export * from "@regexer/coreTypes/MatchTypes";
