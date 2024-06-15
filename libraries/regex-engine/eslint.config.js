6de8083c8d29b7d356e9b4b841b41606bde8ee3f
Kundros <dominik@kundra.cz>
2024-06-15 22:54:35 +0200

    Code inspection, refactoring, dependency checking, npm packages checking, and renamed library name

---
+++ libraries/regex-engine/eslint.config.js
@@ -0,0 +1,32 @@
+const tsEslint = require('typescript-eslint');
+
+module.exports = tsEslint.config(
+    ...tsEslint.configs.strict,
+    ...tsEslint.configs.stylistic,
+    {
+        ignores: [
+            "out",
+            "dist",
+            "node_modules",
+            "**/*.d.ts",
+            "**/*.js"
+        ]
+    },
+    {
+        rules: {
+            "@typescript-eslint/consistent-type-definitions": [
+                "error",
+                "type"
+            ],
+            "@typescript-eslint/consistent-type-assertions": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/no-inferrable-types": "off",
+            "@typescript-eslint/prefer-for-of": "off",
+            "@typescript-eslint/no-var-requires": "off"
+        }
+    },
+    {
+        files: ['**/*.js'],
+        ...tsEslint.configs.disableTypeChecked,
+    }
+);
